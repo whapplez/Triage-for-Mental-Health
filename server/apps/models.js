124d73da7f62b7847b738be022bbd6e04b94138f
Siddharth Joshi <sjoshi804@gmail.com>
2020-06-28 10:03:26 -0700

    fixed error in models

--- server/apps/models.js
+++ server/apps/models.js
@@ -1,12 +1,12 @@
 //Constructor for Patient Response
-exports.PatientResponse(time, phone_number, medium) 
+exports.PatientResponse = (time, phone_number, medium) =>
 {
     this.time = time;
     this.phone_number = phone_number;
     this.medium = medium;
 }
 
-exports.VoiceResponseObject(question_id, recording_url, transcription_text, transcription_url)
+exports.VoiceResponseObject = (question_id, recording_url, transcription_text, transcription_url) =>
 {
     this.question_id = question_id; //which question is this the response to
     this.recording_url = recording_url;
