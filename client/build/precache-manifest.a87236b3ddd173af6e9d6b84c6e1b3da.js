fed6a8d42a8616924e13d8eca162c20b3f70d5a6
Tim Han <whapplez@gmail.com>
2020-06-28 15:29:37 -0400

    FINALLYBUILD

---
+++ client/build/precache-manifest.a87236b3ddd173af6e9d6b84c6e1b3da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72f786c1f97c0be0c3ed43823a4f3cb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71506560361bc52b3b8a",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "958281991e44ffe00872",
+    "url": "/static/js/2.d6dfa273.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6dfa273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71506560361bc52b3b8a",
+    "url": "/static/js/main.40b8bb2a.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
